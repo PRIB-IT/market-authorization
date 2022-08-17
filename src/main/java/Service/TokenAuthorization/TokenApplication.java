7d3ecb87ea97b2522aba2902df45baf50f1df9fc
pablohdl <pablohdl@hotmail.com>
2022-08-16 21:42:50 -0300

    Hello world

---
+++ src/main/java/Service/TokenAuthorization/TokenApplication.java
@@ -0,0 +1,17 @@
+package Service.TokenAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class TokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Service/TokenAuthorization/controller/TokenController.java
@@ -0,0 +1,14 @@
+package Service.TokenAuthorization.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Iniciamos os Trabalhos";
+    }
+}
