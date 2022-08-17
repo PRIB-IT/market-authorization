8fc03d5cd7c50478f6b4706284e8a063a2e99b77
pablohdl <pablohdl@hotmail.com>
2022-08-16 21:49:57 -0300

    Adjustment

--- src/main/java/user/tokenauthorization/TokenApplication.java
+++ src/main/java/user/tokenauthorization/TokenApplication.java
@@ -1,4 +1,4 @@
-package Service.TokenAuthorization;
+package user.tokenauthorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/main/java/user/tokenauthorization/controller/TokenController.java
+++ src/main/java/user/tokenauthorization/controller/TokenController.java
@@ -1,4 +1,4 @@
-package Service.TokenAuthorization.controller;
+package user.tokenauthorization.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
